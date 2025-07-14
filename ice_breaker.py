016ef071c639802bb1d16db215d8c2eb14505597
haideptrai99 <haidaica99999@gmail.com>
2025-07-14 16:29:44 +0700

    connect openai,hugging face,check gpu

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,5 +1,13 @@
 import os
+from langchain_openai import ChatOpenAI
 
-if __name__ == "__main__":
-    print("hello world")
-    print(os.getenv("OPENAI_API_KEY"))
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_BASE = os.getenv("OPENAI_API_BASE")
+llm = ChatOpenAI(
+    model="openai:gpt-4o-mini",
+    openai_api_key=OPENAI_API_KEY,
+    temperature=0.7,
+    base_url=OPENAI_API_BASE,
+)
+response = llm.invoke("Hello, how are you?")
+print(response.content)
